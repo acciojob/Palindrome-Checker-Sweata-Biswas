5c180a8002857ee0dc92688884223d5eef7e58cb
Sweata-Biswas <biswassweata22@gmail.com>
2023-01-07 21:23:05 +0530

    first

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+int length = str.length;
+str1 = str.toLowerCase();
+for(int i =0 ; i< length /2; i++){
+if(str1[i] != str1[length -1-i]){
+return false;
+}
+}
+return true;
 }
 module.exports = palindrome
